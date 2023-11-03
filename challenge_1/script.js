66acfb11f0b24745d0d4da5efe2345ce44177262
Jeonghyeon Kim <142366319+johnkimkong@users.noreply.github.com>
2023-11-03 23:25:22 +1100

    Add files via upload
    Working on the HTML, CSS, and Java script 50 days challenge. This file is the first challenge, but not completed yet.

---
+++ challenge_1/script.js
@@ -0,0 +1,4 @@
+document.getElementById('exp-sydney').addEventListener('click', function() {
+    var sydneyImg = this.querySelector('.card-sydney');
+    sydneyImg.classList.toggle('expanded');
+});
