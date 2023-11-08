d829941aefa9a26426bca8bd9feeb6e74f627746
Jeonghyeon Kim <142366319+johnkimkong@users.noreply.github.com>
2023-11-08 18:31:05 +1100

    QR code challenge

--- index.js
+++ index.js
@@ -1,22 +1,38 @@
-// var generateName = require("sillyname");
+/* 
+1. Use the inquirer npm package to get user input.
+2. Use the qr-image npm package to turn the user entered URL into a QR code image.
+3. Create a txt file to save the user input using the native fs node module.
+*/
 
-// import generateName from "sillyname";
-// var sillyName = generateName(); 
+import inquirer from 'inquirer';
+import qr from "qr-image";
+import fs from 'fs';
 
-// console.log(`My name is ${sillyName}.`); 
+inquirer
+ .prompt([
+  {
+    message: "Type in your URL: ", 
+    name: "URL",
+  },
+])
+  .then((answers) => {
+    const url = answers.URL;
+    var qr_svg = qr.image('url');
+    qr_svg.pipe(fs.createWriteStream('qr_img.png'));
 
-// const superheroes = require('superheroes');
+    fs.writeFile('URL.txt', url, (err) => {
+        if (err) throw err;
+        console.log("The file has been saved!");
+      }); 
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
 
- 
-// superheroes.all;
-// //=> ['3-D Man', 'A-Bomb', …]
- 
-// superheroes.random();
-// //=> 'Spider-Ham'
-
-// console.log(`I am ${superheroes}.`);
-import superheroes from "superheroes";
-
-const name = superheroes.random(); 
-
-console.log(`I am ${name}!`);
+// var qr_svg = qr.image('I love QR!', { type: 'svg' });
+// qr_svg.pipe(require('fs').createWriteStream('i_love_qr.svg'));
+// var svg_string = qr.imageSync('I love QR!', { type: 'svg' });
