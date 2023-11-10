2bbc7368856d04efad6bcf7266cb1ebd0fca14f8
Jeonghyeon Kim <142366319+johnkimkong@users.noreply.github.com>
2023-11-10 23:06:31 +1100

    Studied on the Middleware
    Learning about the middleware was difficult, so I need to revise it multiple times to be familiar with this concept and how to use it.

---
+++ index3.js
@@ -0,0 +1,23 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+// app.use(logger);
+
+function logger(req, res, next) {
+  console.log("Request Method: ", req.method); 
+  console.log("Request URL: ", req.url);
+  next();
+}
+
+// Use the logger middleware in your Express app
+app.use(logger); 
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
